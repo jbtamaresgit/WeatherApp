468f74167f7d04a434ade6e00f432d7630f13073
jbtamaresgit <jbtamaresgit@outlook.com>
2021-11-13 18:22:22 +0800

    Create base weather contract and entity

--- WeatherApp/Contracts/OneCallWeather/HistoricalWeatherContract.cs
+++ WeatherApp/Contracts/OneCallWeather/HistoricalWeatherContract.cs
@@ -1,6 +1,36 @@
-﻿namespace Contracts.OneCallWeather
+﻿using System.Collections.Generic;
+
+namespace Contracts.OneCallWeather
 {
-    public class HistoricalWeatherContract
+    public class HistoricalWeatherContract : BaseWeatherContract
     {
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public string timezone { get; set; }
+        public int timezone_offset { get; set; }
+        public Current current { get; set; }
+        public List<Hourly> hourly { get; set; }
+    }
+
+    public class Rain
+    {
+        public double _1h { get; set; }
+    }
+    public class Hourly
+    {
+        public int dt { get; set; }
+        public double temp { get; set; }
+        public double feels_like { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+        public double dew_point { get; set; }
+        public double uvi { get; set; }
+        public int clouds { get; set; }
+        public int visibility { get; set; }
+        public double wind_speed { get; set; }
+        public int wind_deg { get; set; }
+        public double wind_gust { get; set; }
+        public List<BaseWeatherContract.Weather> weather { get; set; }
+        public Rain rain { get; set; }
     }
 }
--- WeatherApp/Entities/OneCallWeather/CurrentWeatherEntity.cs
+++ WeatherApp/Entities/OneCallWeather/CurrentWeatherEntity.cs
@@ -1,6 +1,32 @@
-﻿namespace Entities.OneCallWeather
+﻿using System.Collections.Generic;
+
+namespace Entities.OneCallWeather
 {
-    public class CurrentWeatherEntity : BaseEntity
+    public class CurrentWeatherEntity : BaseWeatherEntity
     {
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public string timezone { get; set; }
+        public int timezone_offset { get; set; }
+        public Current current { get; set; }
+        public List<Minutely> minutely { get; set; }
+        public List<Alert> alerts { get; set; }
+    }
+
+  
+    public class Minutely
+    {
+        public int dt { get; set; }
+        public int precipitation { get; set; }
+    }
+
+    public class Alert
+    {
+        public string sender_name { get; set; }
+        public string @event { get; set; }
+        public int start { get; set; }
+        public int end { get; set; }
+        public string description { get; set; }
+        public List<string> tags { get; set; }
     }
 }
